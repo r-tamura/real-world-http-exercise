9fdadfe54e758c168e0d10d5c5ee8d1377d629fb
r-tamura <rtamura30@gmail.com>
2017-12-19 23:36:33 +0900

    [go] 6.5 RPC

--- go/server/httpserver.go
+++ go/server/httpserver.go
@@ -74,11 +74,26 @@ func upgradeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func chunkHandler(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/cookie", cookieHandler)
 	http.HandleFunc("/upgrade", upgradeHandler)
+	http.HandleFunc("/chunked", chunkHandler)
 	log.Println("start http listening :18888")
 	httpServer.Addr = "127.0.0.1:18888"
 	log.Println(httpServer.ListenAndServe())
